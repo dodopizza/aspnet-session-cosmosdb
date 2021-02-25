518567bfa7833b66a366b85ca7f5f1672a39353a
George Polevoy <george.polevoy@gmail.com>
2021-02-25 09:15:14 +0300

    Bug locking conflict (#6)
    * retry locking. Closes #4
    
    * fix locking conflict

--- Dodo.AspNet.SessionProviders.CosmosDb/tryLock.js
+++ Dodo.AspNet.SessionProviders.CosmosDb/tryLock.js
@@ -3,32 +3,41 @@ function tryLock(lockId, createdDate, ttl) {
     var collectionLink = collection.getSelfLink();
     var response = getContext().getResponse();
 
-    var query = 'select * from root r where r.id = "' + lockId + '"';
-    collection.queryDocuments(collectionLink, query, {}, function(err, documents) {
-        if (err)
-        {
-            throw err;
-        }
-        if (documents.length > 0)
-        {
-            var existingDocument = documents[0];
-            var responseDoc = {locked: false, etag: existingDocument._etag, createdDate: existingDocument.createdDate };
-            response.setBody(responseDoc);
-        }
-        else
-        {
-            createNewLockRecord();
-        }
-    });
+    queryExistingLock();
+
+    function queryExistingLock() {
+        var query = 'select * from root r where r.id = "' + lockId + '"';
+        var accept = collection.queryDocuments(collectionLink, query, {}, function (err, documents) {
+            if (err) {
+                throw err;
+            }
+            if (documents.length > 0) {
+                var existingDocument = documents[0];
+                response.setBody({
+                    locked: false,
+                    etag: existingDocument._etag,
+                    createdDate: existingDocument.createdDate
+                });
+            } else {
+                createNewLockRecord();
+            }
+        });
+        if (!accept) throw "Unable to query for existing lock";
+    }
 
     function createNewLockRecord() {
         let lockItem = {id: lockId, createdDate: createdDate, ttl: ttl};
-        collection.createDocument(collectionLink, lockItem,
+        var accept = collection.createDocument(collectionLink, lockItem,
             function (err, createdDocument) {
                 if (err) {
                     throw err;
                 }
-                response.setBody({locked: true, etag: createdDocument._etag, createdDate: createdDocument.createdDate});
+                response.setBody({
+                    locked: true,
+                    etag: createdDocument._etag,
+                    createdDate: createdDocument.createdDate
+                });
             });
+        if (!accept) throw "Unable to create a lock";
     }
 }
